04d2149f081999e0bfedbe7c8b1e22379466782b
Lucas Vieira <lucas.vieira.093@ufrn.edu.br>
2022-09-29 16:47:43 -0300

    Iniciando implementação da autenticação da aplicação

--- src/main/java/com/helpdesk/helpdesk/services/TecnicoService.java
+++ src/main/java/com/helpdesk/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.helpdesk.helpdesk.domain.Pessoa;
@@ -26,6 +27,9 @@ public class TecnicoService {
     @Autowired
     private PessoaRepository pessoaRepository;
 
+    @Autowired 
+    private BCryptPasswordEncoder encoder;
+
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id));
@@ -44,6 +48,7 @@ public class TecnicoService {
 
     public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
         objDTO.setId(id);
+        objDTO.setSenha(encoder.encode(objDTO.getSenha()));
         Tecnico oldObj = findById(id);
         validatesCpfAndEmail(objDTO);
         oldObj = new Tecnico(objDTO);
